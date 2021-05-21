f4fb253a8e96cbf5e9f238f9bd8782e47894d92a
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-05-22 01:13:11 +0900

    fixed migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_21_122746) do
+ActiveRecord::Schema.define(version: 2021_05_21_123029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
-    t.string "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "graph_qls", force: :cascade do |t|
-    t.string "title"
-    t.string "content"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
